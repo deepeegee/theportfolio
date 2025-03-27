230bd3d8bf15ce5b2d574ad8a3c2c5c34c7ff1fb
deepeegee <tosinorunesajo@gmail.com>
2025-03-27 23:27:23 +0100

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { SwitchTextTrack } from "./mui-treasury/switch-textTrack";
 import { useMediaQuery } from "@mui/material";
 import DrawerMobileNavigation from "./components/Navbar/Sidebar"
 import TwoTextBoxes from "./components/navabr2";
-import ImageModal from "./components/Rename"
+// import ImageModal from "./components/Rename"
 
 
 function App() {
@@ -40,7 +40,7 @@ function App() {
         <Navbar toggleDarkMode={toggleDarkModeHandler} darkMode={darkMode} />
       )}
       <Hero />
-      <ImageModal />
+      {/* <ImageModal /> */}
       <Experience darkMode={darkMode} />
       <ScrollToTopButton />
       <Footer />
